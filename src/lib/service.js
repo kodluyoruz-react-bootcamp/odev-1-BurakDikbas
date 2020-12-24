ba53f967239b4f27fa850fd9b7fa7b0bc76f75ad
BurakDikbas <68542498+BurakDikbas@users.noreply.github.com>
2020-12-25 00:29:06 +0300

    Update service.js

--- src/lib/service.js
+++ src/lib/service.js
@@ -9,7 +9,7 @@ const ENDPOINT = "https://jsonplaceholder.typicode.com";
 
 async function getData() {
 	try {
-        const { number: userid } = await axios.get(`${ENDPOINT}/users/1`);
+        const { data: userid } = await axios.get(`${ENDPOINT}/users/1`);
         const { data: posts } = await axios.get(`${ENDPOINT}/posts/1`)
 		console.log(userid,posts);
 	} catch (e) {
